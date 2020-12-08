8026aefef8444fe6a213f6f16a7c2ed7185decf8
Jared Flomen <jaredflomen@Jareds-MacBook-Pro-2.local>
2020-12-08 13:56:58 -0500

    Add second story to Appointment with time of 12pm

--- stories/index.js
+++ stories/index.js
@@ -129,3 +129,4 @@ storiesOf("Appointment", module)
   backgrounds: [{ name: "white", value: "#fff", default: true }]
 })
 .add("Appointment", () => <Appointment />)
+.add("Appointment", () => <Appointment time="12pm" />)
