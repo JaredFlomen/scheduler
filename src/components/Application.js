022277321930d81cc6b20ca75b4d37bdbd93dc7a
Jared Flomen <jaredflomen@Jareds-MacBook-Pro-2.local>
2020-12-07 19:19:42 -0500

    Add some semicolons where applicable

--- src/components/Application.js
+++ src/components/Application.js
@@ -32,7 +32,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList
+          <DayList 
             days={days}
             day={day}
             setDay={setDay}
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -7,12 +7,12 @@ export default function DayListItem(props) {
   const dayClass = classnames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0
-  });  
+  });
 
   const formatSpots = (spots) => {
-    if (spots === 0) return "no spots remaining"
-    if (spots === 1) return "1 spot remaining"
-    return `${spots} spots remaining`
+    if (spots === 0) return "No spots remaining";
+    if (spots === 1) return "1 spot remaining";
+    return `${spots} spots remaining`;
   }
 
   return (
