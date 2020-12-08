339dc3db13ddaf05df5ef7871830e88911644f32
Jared Flomen <jaredflomen@Jareds-MacBook-Pro-2.local>
2020-12-08 14:14:29 -0500

    Add Empty story and refactor empty component

--- stories/index.js
+++ stories/index.js
@@ -9,6 +9,7 @@ import InterviewerListItem from 'components/InterviewerListItem';
 import InterviewerList from 'components/InterviewerList';
 import Appointment from 'components/Appointment/index';
 import Header from 'components/Appointment/Header';
+import Empty from 'components/Application/Empty';
 
 storiesOf("Button", module)
   .addParameters({
@@ -132,3 +133,4 @@ storiesOf("Appointment", module)
 .add("Appointment", () => <Appointment />)
 .add("Appointment with Time", () => <Appointment time="12pm" />)
 .add("Header", () => <Header time="12pm" />)
+.add("Empty", () => <Empty onAdd={action("onAdd")} />)
