66c7746a579cdfccaa068e0cd5540ff4e0db01d5
Jared Flomen <jaredflomen@Jareds-MacBook-Pro-2.local>
2020-12-08 19:07:28 -0500

    Iterate over appointments data to fill the schedule

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import DayList from './DayList'
 import "../styles/Application.scss";
+import Appointment from 'components/Appointment';
+import { parse } from "@babel/core";
 
 const days = [
   {
@@ -20,7 +22,59 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+    interview: {
+      student: "Jared Flomen",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "3pm",
+  },
+  {
+    id: "last",
+    time: "4pm",
+    interview: {
+      student: "Lindsay H",
+      interviewer: {
+        id: 3, 
+        name: "Mildred Nazir", 
+        avatar: "https://i.imgur.com/T2WwVfS.png" 
+      }
+    }
+  },
+  {
+    id: "last",
+    time: "5pm",
+  }
+];
+
 export default function Application(props) {
+  const parsedAppointments = appointments.map(appointment => <Appointment key={appointment.id} {...appointment} />);
   const [day, setDay] = useState('Monday');
   return (
     <main className="layout">
@@ -45,7 +99,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements during the "The Scheduler" activity. */}
+        { parsedAppointments }
       </section>
     </main>
   );
