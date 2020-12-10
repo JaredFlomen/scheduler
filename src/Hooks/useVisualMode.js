c611addb6ddc13cadf31446241a120d11c4e44ea
Jared Flomen <jaredflomen@Jareds-MacBook-Pro-2.local>
2020-12-10 14:00:44 -0500

    Keeping track of the mode and going using a back function to go back in history

--- src/Hooks/useVisualMode.js
+++ src/Hooks/useVisualMode.js
@@ -2,10 +2,19 @@ import React, { useState } from 'react';
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  function transition(initial) { 
-    setMode(initial)
+  function transition(newMode) { 
+    setMode(newMode)
+    setHistory(previousHistory => [...previousHistory, newMode])
+  }
+
+  function back() {
+    history.pop()
+    const expectedMode = history[history.length-1]
+    setMode(expectedMode)
+    setHistory(history)
   }
   
-  return { mode, transition }
+  return { mode, transition, back }
 }
