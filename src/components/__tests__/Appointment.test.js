d8869255c031a4d3b10d1a192c5997615322ec3f
Jared Flomen <jaredflomen@gmail.com>
2020-12-14 13:08:07 -0500

    Add base Form test file

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -2,6 +2,19 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Application from 'components/Application';
 
-it('Renders without crashing', () => {
-  render(<Application />);
-});
+describe('Appointment', () => {
+  it('Renders without crashing', () => {
+    render(<Application />);
+  });
+  //Commented it out as I wasn't sure we were supposed to run the tests
+
+  // it('Does not call the function', () => {
+  //   const fn = jest.fn();
+  //   expect(fn).toHaveBeenCalledTimes(0);
+  // });
+  // it("calls the function", () => {
+  //   const fn = jest.fn();
+  //   fn();
+  //   expect(fn).toHaveBeenCalledTimes(1);
+  //  });
+})
