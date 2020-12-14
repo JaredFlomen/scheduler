0801a4f9241d43f7cc7788d888768acb0012428a
Jared Flomen <jaredflomen@gmail.com>
2020-12-14 15:00:12 -0500

    Streamlining the tests and adding an error clear in the Form component

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -36,37 +36,27 @@ describe("Form", () => {
     expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
     expect(onSave).not.toHaveBeenCalled();
   });
-  
-  it("calls onSave function when the name is defined", () => {
-    //Mock onSave button
+
+  it("can successfully save after trying to submit an empty student name", () => {
     const onSave = jest.fn();
-
-    //Renders Form with mock save function and interviewers
-    const { getByText, queryByText } = render(<Form interviewers={interviewers} onSave={onSave} name="Lydia Miller-Jones"/>)
-
-    //Clicking the Save button
-    fireEvent.click(getByText('Save'))
-
+    const { getByText, getByPlaceholderText, queryByText } = render(
+      <Form interviewers={interviewers} onSave={onSave} />
+    );
+  
+    fireEvent.click(getByText("Save"));
+  
+    expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
+    expect(onSave).not.toHaveBeenCalled();
+  
+    fireEvent.change(getByPlaceholderText("Enter Student Name"), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+  
+    fireEvent.click(getByText("Save"));
+  
     expect(queryByText(/student name cannot be blank/i)).toBeNull();
+  
     expect(onSave).toHaveBeenCalledTimes(1);
-
-    //Added a second null as I created onEditCheck in the Form component which is used to calculate spots remaining
     expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null, null);
   });
-
-  it('submits the name entered by the user', () => {
-    const onSave = jest.fn();
-    const { getByText, getByPlaceholderText } = render(
-    <Form interviewers={interviewers} onSave={onSave} 
-    />);
-
-    const input = getByPlaceholderText('Enter Student Name');
-
-    fireEvent.change(input, {target : {value: "Lydia Miller-Jones"} });
-    fireEvent.click(getByText('Save'));
-
-    expect(onSave).toHaveBeenCalledTimes(1);
-    //Added a second null as I created onEditCheck in the Form component which is used to calculate spots remaining
-    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null, null)
-  })
 });
