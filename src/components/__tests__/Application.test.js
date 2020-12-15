78ab6fb1f4971be1a8d6c2bf4188d64e97376cb4
Jared Flomen <jaredflomen@gmail.com>
2020-12-15 13:50:11 -0500

    Adding steps in the jest test to book an interview

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent, prettyDOM } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, prettyDOM, getByText, getAllByTestId, getByAltText, getByPlaceholderText } from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
 describe('Application', () => {
-  it.skip("Defaults to Monday and changes the schedule when a new day is selected", async () => {
+  it("Defaults to Monday and changes the schedule when a new day is selected", async () => {
     const { getByText } = render(<Application />);
   
     await waitForElement(() => getByText("Monday"));
@@ -17,9 +17,17 @@ describe('Application', () => {
     expect(getByText("Leopold Silvers")).toBeInTheDocument();
   });
   
-  it('Loads data, books an interview and reduces the spots remaining for the first day by 1', () => {
+  it('Loads data, books an interview and reduces the spots remaining for the first day by 1', async () => {
     const { container } = render(<Application />);
-    console.log(prettyDOM(container))
+    await waitForElement(() => getByText(container, 'Archie Cohen'));
+    const appointment = getAllByTestId(container, 'appointment')[0]
+    fireEvent.click(getByAltText(appointment, 'Add'))
+    fireEvent.change(getByPlaceholderText(appointment, /Enter Student Name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"))
+    fireEvent.click(getByText(appointment, "Save"));
+    console.log(prettyDOM(appointment))
   })
 })
 
