706a343c3d32f794306c5e8c019992dde1972a27
Jared Flomen <jaredflomen@gmail.com>
2020-12-15 13:08:44 -0500

    Verifying the jest test passes

--- src/Hooks/useApplicationData.js
+++ src/Hooks/useApplicationData.js
@@ -72,10 +72,10 @@ export default function useApplicationData() {
 
   //Resovles many Promieses and handles them
   useEffect(() => {
-    Promise.all([ 
-      axios.get('api/days'),
-      axios.get('api/appointments'),
-      axios.get('api/interviewers')
+    Promise.all([
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
     ]).then((all) => {
       //Updates the days, appointments and interviewers request at the same time
       setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}))
