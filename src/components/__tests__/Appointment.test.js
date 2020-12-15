a37af3a7722ede71ca42b5b821489939954f4ff2
Jared Flomen <jaredflomen@gmail.com>
2020-12-15 13:11:18 -0500

    Removing any render without crashing test

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -3,18 +3,13 @@ import { render } from '@testing-library/react';
 import Application from 'components/Application';
 
 describe('Appointment', () => {
-  it('Renders without crashing', () => {
-    render(<Application />);
+  it('Does not call the function', () => {
+    const fn = jest.fn();
+    expect(fn).toHaveBeenCalledTimes(0);
   });
-  //Commented it out as I wasn't sure we were supposed to run the tests
-
-  // it('Does not call the function', () => {
-  //   const fn = jest.fn();
-  //   expect(fn).toHaveBeenCalledTimes(0);
-  // });
-  // it("calls the function", () => {
-  //   const fn = jest.fn();
-  //   fn();
-  //   expect(fn).toHaveBeenCalledTimes(1);
-  //  });
+  it("calls the function", () => {
+    const fn = jest.fn();
+    fn();
+    expect(fn).toHaveBeenCalledTimes(1);
+   });
 })
