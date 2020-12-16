6a9c7ea97ea4fb27ed7b86a66f9cb6cb82c73d8e
Jared Flomen <jaredflomen@gmail.com>
2020-12-16 13:52:50 -0500

    Deleting an interview

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -16,7 +16,7 @@ describe('Appointments', () => {
     cy.contains('.appointment__card--show', "Sylvia Palmer")
   });
 
-  it('Should edit an interview', () => {
+  xit('Should edit an interview', () => {
     cy.get('[alt=Edit]').first().click({force: true});
     cy.get('[data-testid=student-name-input]').clear();
     cy.get('[data-testid=student-name-input]').type('Jared Flomen')
@@ -24,5 +24,13 @@ describe('Appointments', () => {
     cy.contains('Save').click();
     cy.contains('.appointment__card--show', "Jared Flomen")
     cy.contains('.appointment__card--show', "Tori Malcolm")
+  });
+
+  it('Should cancel an interview', () => {
+    cy.get('[alt=Delete]').first().click({force: true});
+    cy.contains('Confirm').click();
+    cy.contains('Deleting').should('exist');
+    cy.contains('Deleting').should('not.exist');
+    cy.contains('.appointment__card--show', "Archie Cohen").should('not.exist')
   })
 })
