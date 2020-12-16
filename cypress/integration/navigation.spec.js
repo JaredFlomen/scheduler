f8d499322f12e72836c4b1c26315e9aab43f24b1
Jared Flomen <jaredflomen@gmail.com>
2020-12-16 12:53:58 -0500

    Navigating to Tuesday and inspecting the background color

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -2,4 +2,9 @@ describe('Navigation', () => {
   it('Should visit root', () => {
     cy.visit('/');
   });
+  it('Should navigate to Tuesday', () => {
+    cy.contains('li', "Tuesday")
+    .click()
+    .should('have.css', 'background-color', 'rgb(242, 242, 242)')
+  })
 });
