d66e9a1fd09bad8cba87b03b040b661c05ba05b7
Jared Flomen <jaredflomen@gmail.com>
2020-12-16 13:42:41 -0500

    Force click to be true to click edit button

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,4 +1,4 @@
-describe('Should book an interview', () => {
+describe('Appointments', () => {
   beforeEach(() => {
     cy.request("GET", "/api/debug/reset");
   
@@ -7,12 +7,16 @@ describe('Should book an interview', () => {
     cy.contains("Monday");
    });
 
-  it("Should book an interview", () => {
+  xit("Should book an interview", () => {
     cy.get('[alt=Add]').first().click();
     cy.get('[data-testid=student-name-input]').type('Lydia Miller-Jones')
     cy.get("[alt='Sylvia Palmer']").click()
     cy.contains('Save').click();
     cy.contains('.appointment__card--show', "Lydia Miller-Jones")
     cy.contains('.appointment__card--show', "Sylvia Palmer")
+  });
+
+  it('Should edit an interview', () => {
+    cy.get('[alt=Edit]').click({force: true});
   })
 })
