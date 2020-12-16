d714292e925737bf6c912a7650c64ac0fee3f3ad
Jared Flomen <jaredflomen@gmail.com>
2020-12-16 13:20:33 -0500

    Clicking the first add button and typing in a name

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -2,5 +2,7 @@ describe('Should book an interview', () => {
   it("Should book an interview", () => {
     cy.visit('/');
     cy.contains("Monday");
+    cy.get('[alt=Add]').first().click();
+    cy.get('[data-testid=student-name-input]').type('Lydia Miller-Jones')
   })
 })
