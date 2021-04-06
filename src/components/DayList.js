26400394c8c578c14af9778db8eb655a16f66a20
Jared Flomen <jaredflomen@gmail.com>
2021-04-06 12:16:24 -0400

    Update DayList.js

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import DayListItem from 'components/DayListItem';
 
-export default function DayList(props) {
-  const { days } = props;
+export default function DayList({ days, day, setDay }) {
   const parsedDayList = days.map(day => (
     <DayListItem
       key={day.id}
       name={day.name}
       spots={day.spots}
-      selected={day.name === props.day}
-      setDay={props.setDay}
+      selected={day.name === day}
+      setDay={setDay}
     />
   ));
   return <ul>{parsedDayList}</ul>;
