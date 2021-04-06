da80bd87d916f5119224ed37f9b591c50b557ae8
Jared Flomen <jaredflomen@gmail.com>
2021-04-06 12:11:41 -0400

    Destructure show component

--- src/components/Appointment/Show.js
+++ src/components/Appointment/Show.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
-export default function Show(props) {
+export default function Show({
+  student,
+  interviewer,
+  cancelInterview,
+  editInterview,
+}) {
   return (
     <main className='appointment__card appointment__card--show'>
       <section className='appointment__card-left'>
-        <h2 className='text--regular'>{props.student}</h2>
+        <h2 className='text--regular'>{student}</h2>
         <section className='interviewer'>
           <h4 className='text--light'>Interviewer</h4>
-          <h3 className='text--regular'>{props.interviewer}</h3>
+          <h3 className='text--regular'>{interviewer}</h3>
         </section>
       </section>
       <section className='appointment__card-right'>
@@ -16,13 +21,13 @@ export default function Show(props) {
             className='appointment__actions-button'
             src='images/edit.png'
             alt='Edit'
-            onClick={props.editInterview}
+            onClick={editInterview}
           />
           <img
             className='appointment__actions-button'
             src='images/trash.png'
             alt='Delete'
-            onClick={props.cancelInterview}
+            onClick={cancelInterview}
           />
         </section>
       </section>
